114be0f55b981c62b855a4bcb910faf907413f83
Fernando <fernando.luiz92@hotmail.com>
2022-07-02 22:30:41 -0300

    Associando informações entre componentes e template com Data Binding

--- workspace/arquitetura-angular/src/app/app.module.ts
+++ workspace/arquitetura-angular/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
-    NewComponentComponent
+    NewComponentComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
